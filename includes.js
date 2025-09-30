517a35ad385a7266c9ddbdf138b6ccc87b2f2bba
BitterGuitar3 <andrewdonaldjohnson@comcast.net>
2025-09-30 18:33:38 -0400

    Quality of management update
    Added 3 different HTML files that contain the HTML needed for making the header and foot of each page. Use Javascript to get the header and footer elements to apply the HTML in those elements. This allows me to only need to update one of those 3 html files to make changes across multiple pages instead of having to go to each one individually.

---
+++ includes.js
@@ -0,0 +1,11 @@
+function loadHTML(selector, file) {
+    fetch(file) //Load the file (e.g, includes/header.html)
+        .then(response => {
+            if(!response.ok) throw new Error(`Could not load ${file}`);
+            return response.text(); //Conver the response to plain text (HTML)
+        })
+        .then(data => {
+            document.querySelector(selector).innerHTML = data; //Insert it into the page
+        })
+        .catch(error => console.error(error)); //Handle errors (like file not found)
+}
