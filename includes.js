853e47ae6c541d652139904db1b6e6a759abdc0b
BitterGuitar3 <andrewdonaldjohnson@comcast.net>
2026-02-08 18:24:31 -0500

    Updated resume and footer

--- includes.js
+++ includes.js
@@ -2,7 +2,7 @@ function loadHTML(selector, file) {
     fetch(file) //Load the file (e.g, includes/header.html)
         .then(response => {
             if(!response.ok) throw new Error(`Could not load ${file}`);
-            return response.text(); //Conver the response to plain text (HTML)
+            return response.text(); //Convert the response to plain text (HTML)
         })
         .then(data => {
             document.querySelector(selector).innerHTML = data; //Insert it into the page
